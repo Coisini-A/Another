43e09194e32fc6fa020666755b1810ef5849f765
vip <email@example.com>
2019-11-04 19:27:05 +0800

    项目初始化

---
+++ src/components/tab-bar/index.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "tab-bar"
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="banner"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "index"
+  }
+</script>
+
+<style lang="less" scoped>
+  .container {
+    height: 100%;
+
+    .banner {
+      height: 200px;
+      background: aqua;
+    }
+  }
+</style>
